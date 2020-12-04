df1c96f4b751076cae6ddf0c5e84902f756f27f7
Dylan l <45293702+didi955@users.noreply.github.com>
2020-12-04 11:33:32 +0100

    Update SQL.py

--- SQL.py
+++ SQL.py
@@ -1,23 +1,34 @@
 import sqlite3
 
 
-def updatep(connection: sqlite3.Connection, qry: str, *args):
+def update(connection: sqlite3.Connection, qry: str, *args):
     cursor = connection.cursor()
-    for arg in args:
-        cursor.execute(qry, arg)
+    if len(args) == 0:
+        cursor.execute(qry)
+    else:
+        for arg in args:
+            cursor.execute(qry, arg)
     connection.commit()
     cursor.close()
 
 
-def update(connection: sqlite3.Connection, qry: str):
+def queryAll(connection: sqlite3.Connection, qry: str, *args) -> list:
     cursor = connection.cursor()
-    cursor.execute(qry)
-    connection.commit()
-    cursor.close()
-
-
-def query(connection: sqlite3.Connection, qry: str) -> list:
-    cursor = connection.cursor()
-    cursor.execute(qry)
+    if len(args) == 0:
+        cursor.execute(qry)
+    else:
+        for arg in args:
+            cursor.execute(qry, arg)
     rs = cursor.fetchall()
+    return rs
+
+
+def queryOne(connection: sqlite3.Connection, qry: str, *args) -> list:
+    cursor = connection.cursor()
+    if len(args) == 0:
+        cursor.execute(qry)
+    else:
+        for arg in args:
+            cursor.execute(qry, arg)
+    rs = cursor.fetchone()
     return rs
